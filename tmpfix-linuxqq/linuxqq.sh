35bbf4d23e41cd69d0b32ccdbbe4eb2559c4b670
jiayaohuang <huangjiayao_1992@163.com>
2023-12-25 09:24:16 +0800

    remove package: tmpfix-linuxqq

--- tmpfix-linuxqq/linuxqq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -d ~/.config/QQ/versions ]; then
-	find ~/.config/QQ/versions -name sharp-lib -type d -exec rm -r {} \; 2>/dev/null
-fi
-
-LD_PRELOAD=/usr/lib/libcurl.so.4.7.0 /opt/QQ/qq "$@"
