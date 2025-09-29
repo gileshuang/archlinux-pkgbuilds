d556ec9bcd196134fb9d6d7fbe26e3cc184c3a1d
jiayaohuang <huangjiayao_1992@163.com>
2025-09-29 17:17:23 +0800

    tencent-docs-electron-bin: update to 3.10.10-1 with electron34

--- tencent-docs-electron-bin/get-PKGBUILD.sh
+++ tencent-docs-electron-bin/get-PKGBUILD.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-wget 'https://gitlab.archlinux.org/archlinux/packaging/packages/mpv/-/raw/main/PKGBUILD' -O PKGBUILD-mpv
-
 MAKE_DIR=$(dirname $0)
 cd ${MAKE_DIR}
 AUR_PKGNAME=$(basename $(pwd))
@@ -11,6 +9,6 @@ AUR_FILELIST=(
     tencent-docs-electron.sh
 )
 
-wget "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=${AUR_PKGNAME}" -O aur.PKGBUILD
-wget "https://aur.archlinux.org/cgit/aur.git/plain/tencent-docs-electron.sh?h=${AUR_PKGNAME}" -O aur.tencent-docs-electron.sh
-
+for F in ${AUR_FILELIST[@]}; do
+    wget "https://aur.archlinux.org/cgit/aur.git/plain/${F}?h=${AUR_PKGNAME}" -O aur.${F}
+done
